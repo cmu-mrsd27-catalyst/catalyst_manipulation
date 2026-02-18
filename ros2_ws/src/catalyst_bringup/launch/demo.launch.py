ceb5be7a4ba6a20138a2d6e3d07a8374c17aa512
AmanTambi <a.amantambi@gmail.com>
2026-02-18 10:09:34 -0500

    minor

--- ros2_ws/src/catalyst_bringup/launch/demo.launch.py
+++ ros2_ws/src/catalyst_bringup/launch/demo.launch.py
@@ -136,9 +136,9 @@ def generate_launch_description():
 
     trajectory_execution = {
         'moveit_manage_controllers': False,
-        'trajectory_execution.allowed_execution_duration_scaling': 1.2,
-        'trajectory_execution.allowed_goal_duration_margin': 0.5,
-        'trajectory_execution.allowed_start_tolerance': 0.01,
+        'trajectory_execution.allowed_execution_duration_scaling': 2.0,
+        'trajectory_execution.allowed_goal_duration_margin': 1.0,
+        'trajectory_execution.allowed_start_tolerance': 0.0,
     }
 
     moveit_controllers = {
